20855413faeeb4ef16f3a46cff2f16fffab91cd5
Stephen Wrathall <stephen.wrathall@agworld.com.au>
2016-02-04 15:52:47 +0800

    Finish README and implement season param passing
    WEB-21660 #time 2h 30m

--- lib/agworld_api.rb
+++ lib/agworld_api.rb
@@ -1,4 +1,7 @@
+require 'json_api_client'
+
 require_relative 'agworld_api/version'
+require_relative 'agworld_api/query/additional_param_builder'
 require_relative 'agworld_api/endpoints/base'
 require_relative 'agworld_api/endpoints/company'
 require_relative 'agworld_api/endpoints/farm'
--- lib/agworld_api/endpoints/base.rb
+++ lib/agworld_api/endpoints/base.rb
@@ -1,8 +1,12 @@
-require 'json_api_client'
-
 module AgworldApi
   module Endpoints
     class Base < JsonApiClient::Resource
+      class << self
+        extend Forwardable
+        def_delegators :_new_scope, :with_params
+      end
+
+      self.query_builder = AgworldApi::Query::AdditionalParamBuilder
     end
   end
 end
